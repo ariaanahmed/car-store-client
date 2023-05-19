6192107e17317409f3f4fee0c9fc60e3401992ed
ariaan ahmed <you@example.com>
2023-05-19 11:44:21 +0600

    login and sign up page created

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
 const Login = () => {
     return (
-        <div>
-            This is login here
-        </div>
+        <form>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="Your email" name="email" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="Password" name="password" required className="input input-bordered" />
+                                <Link to='/registration' className="label-text-alt link link-hover py-3">New to Site? Login!</Link>
+                            </div>
+                            <div className="form-control">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     );
 };
 
--- src/components/pages/Registration.jsx
+++ src/components/pages/Registration.jsx
@@ -1,10 +1,32 @@
+import { Link } from "react-router-dom";
+
 const Registration = () => {
     return (
         <form>
-           <input type="text" name="name" id="name" placeholder="Name" />
-           <input type="email" name="email" id="email" placeholder="Your email" />
-           <input type="password" name="password" id="password" placeholder="Your password" />
-           <input type="text" name="photoURL" id="photoURL" placeholder="Photo URL" />
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <input type="text" placeholder="Name" name="name" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <input type="text" placeholder="Photo URL" name="photoURL" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <input type="email" placeholder="Your email" name="email" required className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <input type="password" placeholder="Password" name="password" required className="input input-bordered" />
+                                <Link to='/login' className="label-text-alt link link-hover py-3">Already have an account?</Link>
+                            </div>
+                            <div className="form-control">
+                                <button className="btn btn-primary">sign up</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </form>
     );
 };
