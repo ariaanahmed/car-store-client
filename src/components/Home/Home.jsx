72b3d26c0a89b988b581e880adfc8de52a15e7e1
ariaan ahmed <you@example.com>
2023-05-19 22:07:50 +0600

    banner has been added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,6 @@ import Banner from "../banner/Banner";
 const Home = () => {
     return (
         <div>
-            this is home
             <Banner/>
         </div>
     );
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Banner = () => {
-    return (
-        <div>
-            <h3>This is banner</h3>
+  return (
+    <div>
+      <div className="relative">
+        <div className="bg-[url('https://i.postimg.cc/ZYBRJjXW/banner.jpg')] w-full bg-cover bg-center p-24">
+          <div className="absolute inset-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]"></div>
+          <h1 className="text-white uppercase md:text-3xl font-semibold tracking-wider relative z-20">Unleash the Joy <br /> of Play with Toy!</h1>
+          <Link to="/AllToys">
+            <button className="btn btn-outline btn-warning z-30 absolute bottom-10">all toys</button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
