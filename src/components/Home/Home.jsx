80ebd513761d8dfa539d4d24b9fe9c4fbae5f738
ariaan ahmed <you@example.com>
2023-05-20 10:31:08 +0600

    firebase config keys are hidden by .env.local file. and added this file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../banner/Banner";
 import Gellary from "../pages/Gellary";
+import ShopByCategory from "./ShopByCategory";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Gellary/>
+            <ShopByCategory/>
         </div>
     );
 };
---
+++ src/components/Home/ShopByCategory.jsx
@@ -0,0 +1,28 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+const ShopByCategory = () => {
+    return (
+        <div className='pt-10'>
+            <Tabs className="text-center">
+                <TabList>
+                    <Tab>Regular Cars</Tab>
+                    <Tab>Trucks</Tab>
+                    <Tab>Sports Cars</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                </TabPanel>
+            </Tabs>
+        </div>
+    );
+};
+
+export default ShopByCategory;
