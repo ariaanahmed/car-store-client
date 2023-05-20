41ff2441cde9880741e1174239fa6dd6bf00bd22
ariaan ahmed <you@example.com>
2023-05-20 16:38:49 +0600

    tab section added

--- src/components/Home/SportCars.jsx
+++ src/components/Home/SportCars.jsx
@@ -5,15 +5,13 @@ const SportCars = () => {
     const sportCars = useLoaderData();
     const newArryOfDatas = sportCars.Other;
     console.log(newArryOfDatas)
-
     return (
         <div>
             <h2 className='font-bold py-5 text-4xl uppercase text-center'>Sport Cars Details</h2>
             <img className='block mx-auto' src={sportCars.img} alt="" />
             <div className='flex-row md:flex container mx-auto gap-5'>
                 {
-                    newArryOfDatas.map((data) => {
-                        console.log(data)
+                    newArryOfDatas.map((data,) => {
                         return (
                             <div className='card w-full border bg-base-100 shadow-xl mx-auto mb-10' key={data.idx}>
                                 <figure className="px-10 pt-10">
